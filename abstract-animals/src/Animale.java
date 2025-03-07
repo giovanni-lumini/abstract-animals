abcd0c8bf9d51d203a99f8ae43bc52b6a831afb9
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-03-07 15:08:13 +0100

    abstract class-Animale

--- abstract-animals/src/Animale.java
+++ abstract-animals/src/Animale.java
@@ -1,3 +1,14 @@
-public class Animale {
+//abstract class
+public abstract class Animale {
+
+    // metodo normale (con corpo)
+    public void dormi() {
+        System.out.println("Zzz");
+    };
+
+    // metodi abstract (senza corpo)
+    public abstract void verso();
+
+    public abstract void mangia();
 
 }
