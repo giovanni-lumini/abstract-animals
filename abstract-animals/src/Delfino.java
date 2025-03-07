7b93192898955edabc77641d67c46cdf39f2d0ec
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-03-07 15:19:07 +0100

    Class Delfino

--- abstract-animals/src/Delfino.java
+++ abstract-animals/src/Delfino.java
@@ -1,3 +1,22 @@
-public class Delfino {
+public class Delfino extends Animale {
 
+    // constructor
+    public Delfino(String nome, int eta) {
+        super(nome, eta);
+    }
+
+    // metodo normale (con corpo)
+    public void dormi() {
+        System.out.println("Zzz");
+    };
+
+    @Override
+    public void verso() {
+        System.out.println("verso del delfino");
+    }
+
+    @Override
+    public void mangia() {
+        System.out.println("Il delfino mangia i pesci");
+    }
 }
